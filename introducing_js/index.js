cf65c1179745191fb26eaf67ab9d76dd668a9720
Florian Kopp <floriankopp@posteo.de>
2017-11-25 11:15:58 +0100

    Completed 3.14

---
+++ introducing_js/index.js
@@ -0,0 +1,24 @@
+document.getElementById('example').innerHTML = "Javascript rules!"
+
+var a, b; // variables
+a = 'hello'; // statements
+b = a + "world";
+// b = 'hello world'
+
+a = 1; // string
+b = a + 5.5;
+
+/* comment on multiple lines
+// b = 6.5;
+*/
+
+var c = 30; // c: identifier
+/* Rules for identifiers
+var var = 30; // don't reuse keywords
+var 1a = 30; // have to start with letter, _ or $
+var $_a = 30; // works
+var first greeting = 30; // no whitespace
+var first_greeting = 30;
+var firstGreeting = 30;
+
+*/
